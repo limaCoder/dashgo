355e42ab1d7d8d33a2afd76d787394c66e42a3a7
Mario Lima <marioaulima15@outlook.com>
2023-03-13 21:29:03 -0300

    application finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
 import { Flex, Button, Stack } from '@chakra-ui/react'
+import { SubmitHandler, useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+
 import { Input } from '../components/Form/Input'
 
-export default function Home() {
+type SignInFormData = {
+  email: string;
+  password: string;
+}
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('Email obrigatório').email('E-mail inválido'),
+  password: yup.string().required('Senha obrigatória')
+})
+
+export default function SignIn() {
+  const { register, handleSubmit, formState: { errors, isSubmitting} } = useForm<SignInFormData>({
+    resolver: yupResolver(signInFormSchema)
+  })
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
+    await new Promise(resolve => setTimeout(resolve, 2000))
+
+    console.log(values)
+  }
+
   return (
     <Flex 
       w="100vw"
@@ -17,13 +41,34 @@ export default function Home() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" />
-          <Input name="passoword" type="password" label="Senha" />
+          <Input 
+            name="email" 
+            type="email" 
+            label="E-mail"
+            error={errors.email}
+            {...register('email')}
+          />
+          <Input
+            name="passoword" 
+            type="password" 
+            label="Senha"
+            error={errors.password}
+            {...register('password')}
+          />
         </Stack>
 
-        <Button type="submit" mt="6" colorScheme="pink">Entrar</Button>
+        <Button 
+          type="submit" 
+          mt="6" 
+          colorScheme="pink"
+          size='lg'
+          isLoading={isSubmitting}
+        >
+            Entrar
+        </Button>
       </Flex>
     </Flex>
   )
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,98 +1,141 @@
-import { Box, Button, Flex, Heading, Icon, Table, Th, Thead, Tr, Checkbox, Tbody, Td, Text, useBreakpointValue } from '@chakra-ui/react'
-import { RiAddLine } from 'react-icons/ri';
-import Link from 'next/link'
+import { useState } from 'react';
 
-import { Header } from '../../components/Header';
-import { Sidebar } from '../../components/Sidebar';
-import { Pagination } from '../../components/Pagination';
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Icon,
+  Table,
+  Th,
+  Thead,
+  Tr,
+  Checkbox,
+  Tbody,
+  Td,
+  Text,
+  useBreakpointValue,
+  Spinner,
+  Link
+} from "@chakra-ui/react";
+
+import { RiAddLine } from "react-icons/ri";
+import NextLink from 'next/link'
+
+import { Header } from "../../components/Header";
+import { Sidebar } from "../../components/Sidebar";
+import { Pagination } from "../../components/Pagination";
+
+import { useUsers } from '../../services/hooks/useUsers';
+
+import { queryClient } from '../../services/queryClient';
+import { api } from '../../services/api';
+
+type User = {
+  id: string;
+  name: string;
+  email: string;
+  createdAt: string;
+}
 
 export default function UserList() {
+  const [page, setPage] = useState(1)
+  const { data, isLoading, isFetching, error } = useUsers(page);
+  
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
-  })
+  });
+
+  async function handlePrefetchUser(userId: string) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const res = await api.get(`users/${userId}`)
+
+      return res.data
+    }, {
+      staleTime: 1000 * 60 * 10, // 10 minutos
+    })
+  }
 
   return (
     <Box>
       <Header />
 
-      <Flex w='100%' my='6' maxWidth={1480} mx='auto' px={['4', '4', '6']}>
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px={["4", "4", "6"]}>
         <Sidebar />
 
-        <Box flex='1' borderRadius={8} bg='gray.800' p='8'>
-          <Flex mb='8' justify='space-between' align='center'>
-            <Heading size='lg' fontWeight='normal'>Usuários</Heading>
+        <Box flex="1" borderRadius={8} bg="gray.800" p="8">
+          <Flex mb="8" justify="space-between" align="center">
+            <Heading size="lg" fontWeight="normal">
+              Usuários
+              {!isLoading && isFetching && <Spinner size='sm' color='gray=500' ml='4' />}
+            </Heading>
 
-            <Link href='/users/create' passHref>
-              <Button 
-                as='a' 
-                size='sm' 
-                fontSize='sm' 
-                colorScheme='pink'
-                leftIcon={<Icon as={RiAddLine} fontSize='20' />}
+            <NextLink href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
               >
                 Criar novo
               </Button>
-            </Link>
+            </NextLink>
           </Flex>
 
-          <Table colorScheme='whiteAlpha'>
-            <Thead>
-              <Tr>
-                <Th px={['4', '4', '6']} color='gray.300' width='8'>
-                  <Checkbox colorScheme='pink' />
-                </Th>
-                <Th>Usuário</Th>
-                {isWideVersion && <Th>Data de cadastro</Th>}
-              </Tr>
-            </Thead>
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner />
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários;</Text>
+            </Flex>
+          ) : (
+            <>
+              <Table colorScheme="whiteAlpha">
+                <Thead>
+                  <Tr>
+                    <Th px={["4", "4", "6"]} color="gray.300" width="8">
+                      <Checkbox colorScheme="pink" />
+                    </Th>
+                    <Th>Usuário</Th>
+                    {isWideVersion && <Th>Data de cadastro</Th>}
+                  </Tr>
+                </Thead>
 
-            <Tbody>
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Mario Lima</Text>
-                    <Text fontSize='sm' color='gray.300'>marioaulima15@outlook.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-              </Tr>
+                <Tbody>
+                  {data.users.map((user: User) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme='pink' />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Link color='purple.400' onMouseEnter={() => handlePrefetchUser(user.id)}>
+                              <Text fontWeight='bold'>{user.name}</Text>
+                            </Link>
+                            <Text fontSize='sm' color='gray.300'>{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                      </Tr>
+                    )
+                  })}
+                </Tbody>
+              </Table>
 
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Mario Lima</Text>
-                    <Text fontSize='sm' color='gray.300'>marioaulima15@outlook.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-              </Tr>
-
-              <Tr>
-                <Td px={['4', '4', '6']}>
-                  <Checkbox colorScheme='pink' />
-                </Td>
-                <Td>
-                  <Box>
-                    <Text fontWeight='bold'>Mario Lima</Text>
-                    <Text fontSize='sm' color='gray.300'>marioaulima15@outlook.com</Text>
-                  </Box>
-                </Td>
-                {isWideVersion && <Td>04 de Abril, 2021</Td>}
-              </Tr>
-            </Tbody>
-          </Table>
-
-          <Pagination />
+              <Pagination
+                totalCountOfRegisters={data.totalCount}
+                currentPage={page}
+                onPageChange={setPage}
+              />
+            </>
+          )}
         </Box>
       </Flex>
-
     </Box>
   );
-} 
+}
